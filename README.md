5c04d4d21b7b5362d65a8b93987b274a638036b9
Chris Reisor <creisor@gmail.com>
2017-09-14 14:21:09 -0500

    adds dotfiles and a linking script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # dotfiles
 A collection of dotfiles
+
+## link_dotfiles.sh
+Run this when pulling to add new dotfile symlinks.
---
+++ link_dotfiles.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#DOTFILES=(bashrc inputrc)
+DOTFILES=(files/*)
+
+SOURCE_DIR=~/git/dotfiles/files
+
+pushd ~ > /dev/null 2>&1
+for filename in ${DOTFILES[@]}; do
+  f=$(basename $filename)
+  if [ ! -e .${f} ]; then
+    ln -s ${SOURCE_DIR}/${f} .${f}
+  else
+    if [ ! -L .${f} ]; then
+      echo "~/.${f} is not a symlink; skipping"
+    fi
+  fi
+done
+popd > /dev/null 2>&1
